fd2a79e1fe8a98ffdb6f52be1d75c9bb13514b37
Valéria <valeriamenezes022@gmail.com>
2022-07-05 18:44:29 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function colorSelected() {
 }
 function elementClick(event) {
   let classSelected = document.getElementsByClassName('color');
-  console.log(classSelected);
+  // console.log(classSelected);
   for (let index = 0; index < classSelected.length; index += 1) {
     classSelected[index].classList.remove('selected');
   }
@@ -20,3 +20,11 @@ function elementClick(event) {
 colorSelected();
 
 //8
+function pintar() {
+  let pixelBoard = document.querySelector('#pixel-board')
+   pixelBoard.addEventListener('click', function(event){
+    let classSelected = document.querySelector('.selected')
+     event.target.style.backgroundColor = window.getComputedStyle(classSelected).backgroundColor;
+   })
+   }
+pintar();
