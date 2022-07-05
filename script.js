a652d2c7baba3f63b9789089f8ee226dc0af5935
Valéria <valeriamenezes022@gmail.com>
2022-07-05 11:55:41 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+//6
 let colorBlack = document.querySelector('div');
 colorBlack.classList.add('selected');
+
+//7
+function colorSelected() {
+  let classSelected = document.getElementsByClassName('color');
+  for (let index = 0; index < classSelected.length; index += 1) {
+  classSelected[index].addEventListener('click', elementClick)
+}
+}
+function elementClick(event) {
+  let classSelected = document.getElementsByClassName('color');
+  console.log(classSelected);
+  for (let index = 0; index < classSelected.length; index += 1) {
+  classSelected[index].classList.remove('selected');
+}
+event.target.classList.add('selected');
+}
+colorSelected();
